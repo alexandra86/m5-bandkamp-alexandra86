3185ee30ae8327599d0ac42224af7ccdd06205bb
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-04-18 13:07:19 -0300

    feat: tarefa 2 finalizada

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# SECRET_KEY = "django-e91823y78dgasdas89cvyxz76the912j321321-cx9!!#1232111!!!"
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
