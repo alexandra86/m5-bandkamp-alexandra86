6a1d7d815f51230e2a1e7d931fc773ba07b9a7bd
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-04-25 12:40:29 -0300

    fix: reatirados os def creates do serializer de songs e albums

--- albums/serializers.py
+++ albums/serializers.py
@@ -4,9 +4,6 @@ from .models import Album
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        return Album.objects.create(**validated_data)
-
     class Meta:
         model = Album
         fields = ["id", "name", "year", "user_id"]
--- songs/serializers.py
+++ songs/serializers.py
@@ -4,9 +4,6 @@ from .models import Song
 
 
 class SongSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        return Song.objects.create(**validated_data)
-
     class Meta:
         model = Song
         fields = ["id", "title", "duration", "album_id"]
